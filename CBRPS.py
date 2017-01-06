516e086217e4319392eac7679273b33c07dcf29f
Andy Deveaux <andy.deveaux@gmail.com>
2017-01-06 01:35:32 -0400

    Taking a tiny update to CBRPS.py from the gh-pages branch.

--- CBRPS.py
+++ CBRPS.py
@@ -5,7 +5,7 @@ v1.0
 
 Scrapes radio playlist data from some of the Cape Breton radio stations (as of December 2016)
 
-Last Updated: 01/01/2017
+Last Updated: 01/05/2017
 """
 
 # This script is a bit of a mess that I hacked together while learning about Python and its various libraries, so forgive the suckiness
@@ -362,7 +362,7 @@ class DBO:
 							station_id INTEGER NOT NULL REFERENCES stations(id) ON DELETE CASCADE ON UPDATE CASCADE,
 							song_id INTEGER NOT NULL REFERENCES songs(id) ON DELETE CASCADE ON UPDATE CASCADE,
 							original_time TEXT NOT NULL, -- Time scraped by the playlist
-							formatted_time TEXT NOT NULL, -- Time converted by the script in order to regularize all of the times into a specific display format *Stored in Atlantic Standard Time (UTC -04:00)*
+							formatted_time TEXT NOT NULL, -- Time converted by the script in order to regularize all of the times into a specific display format
 							posix_timestamp DATE NOT NULL, -- Easier to sort by *Stored in UTC-0000*
 							created DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 						)""")
